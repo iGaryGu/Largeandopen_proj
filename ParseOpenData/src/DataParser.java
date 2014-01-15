c5c430160a5abcf5beba264c840d0dd1e3399355
iGaryGu <gary640201a@gmail.com>
2014-01-15 22:11:09 +0800

    test ci merge

--- ParseOpenData/src/DataParser.java
+++ ParseOpenData/src/DataParser.java
@@ -105,6 +105,7 @@ public class DataParser {
                 	farmPollution();
                 }
                 writePollutionIntoFile();
+				System.out.println("all end");
                 
         }
         
