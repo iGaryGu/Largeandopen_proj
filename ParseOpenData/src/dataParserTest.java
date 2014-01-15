1e85572506efcc15fb1c72dfe76b3b12255a37fb
iGaryGu <gary640201a@gmail.com>
2014-01-15 23:14:47 +0800

    add unit test

--- ParseOpenData/src/dataParserTest.java
+++ ParseOpenData/src/dataParserTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +25,14 @@ public class dataParserTest {
 	public void testGetAddr() {
 		String str = "711臺南市歸仁區七甲村丁厝街52巷63弄25號";
 		String temp = ParseUrl.getAddr("COAF0001");		
+		assertEquals(str,temp);
+	}
+
+	@Test
+	public void testReadFile() throws IOException {
+		String str = "hello girl\n";
+		String temp = DataParser.readFileToString("test.txt");
+		System.out.println(temp);
 		assertEquals(str,temp);
 	}
 
